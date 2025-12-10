8d0f6802a95fde1ec8b4d332a4588d1231342cc1
Gabriel Varas <gabrielvaras.ibaceta@gmail.com>
2025-12-10 20:43:23 -0300

    frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import { connectDB } from './config/database';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import walletRoutes from './routes/wallet.routes';
 import rouletteRoutes from './routes/roulette.routes';
+
+const FRONTEND_DIST_PATH = path.join(__dirname, '..', '..', 'frontend', 'dist');
 
 dotenv.config();
 connectDB();
@@ -28,5 +31,11 @@ app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', walletRoutes);
 app.use('/api', rouletteRoutes);
+
+app.use(express.static(FRONTEND_DIST_PATH));
+
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(FRONTEND_DIST_PATH, 'index.html'));
+});
 
 export default app;
